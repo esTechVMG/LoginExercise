076e54869d82cab6d214dafa4b1643a73c8e3e3f
Vicente Maroto <vicente.maroto@escuelaestech.es>
2020-10-21 19:14:55 +0200

    Initial Commit

---
+++ LoginExercise/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  LoginExercise
+//
+//  Created by A4-iMAC01 on 21/10/2020.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LoginExercise/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  LoginExercise
+//
+//  Created by A4-iMAC01 on 21/10/2020.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
